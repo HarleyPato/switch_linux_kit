1f8406daf0c9ce5027093cafb0c6d1e065cb061e
Chris Jones <cmsj@tenshu.net>
2018-05-02 13:51:24 +0100

    Silence pushd/popd in exploit script

--- 02_boot_linux.sh
+++ 02_boot_linux.sh
@@ -5,7 +5,7 @@ set -u
 LOGFILE="../exploit.log"
 echo "" > "${LOGFILE}"
 
-pushd product
+pushd product >/dev/null
     echo "Exploiting Switch recovery mode and loading coreboot..."
     sudo ./shofel2.py cbfs.bin coreboot.rom >> "${LOGFILE}" 2>&1
     echo "Waiting for Switch to run coreboot..."
@@ -15,4 +15,4 @@ pushd product
     echo "Sending u-boot script..."
     sudo ./imx_usb -c . >> "${LOGFILE}" 2>&1
     echo "Switch should boot Linux momentarily."
-popd
+popd >/dev/null
